4ecfd2405b5df0f865e8b57236f7c92626cb26f1
Akshay <akshay.kumar3851@gmail.com>
2019-09-24 22:53:56 +0530

    Updates

---
+++ precache-manifest.de7f95e991a156c10504549028b19e7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085a357afa82952e36ca32247cf09a42",
+    "url": "/myreads-book-app/index.html"
+  },
+  {
+    "revision": "dbcacc1cc86e2af7da50",
+    "url": "/myreads-book-app/static/css/main.cbda8831.chunk.css"
+  },
+  {
+    "revision": "e2a004698f2931cbbbdd",
+    "url": "/myreads-book-app/static/js/2.e42dc214.chunk.js"
+  },
+  {
+    "revision": "dbcacc1cc86e2af7da50",
+    "url": "/myreads-book-app/static/js/main.3da9093f.chunk.js"
+  },
+  {
+    "revision": "a943b6eac91d88798419",
+    "url": "/myreads-book-app/static/js/runtime-main.f7644273.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-book-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-book-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-book-app/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
